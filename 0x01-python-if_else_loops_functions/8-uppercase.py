d3c219debc46eda280040322642b96a9419acdd6
chimekinglsey <onlinekingsley@gmail.com>
2023-01-04 08:42:17 +0000

    IsCase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # islower - a function that determines case of input string
 # @c: input string to check
-
-def islower(c):
-    for n in range(97, 122):
-        if ord(c):
+def uppercase(str):
+	 for n in range(65, 98):
+        if ord(str):
             break
