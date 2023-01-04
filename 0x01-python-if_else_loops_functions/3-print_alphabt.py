b2d480541667c732b477b751d71f66b583b53c88
chimekinglsey <onlinekingsley@gmail.com>
2023-01-04 08:05:26 +0000

    nEW COMMIt

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 for i in range(97,123):
-	if i == 101:
-		i += 1
-		continue
+    if i == 101:
+        i += 1
+        continue
 
-	if i == 113:
-		i += 1
-		continue
+    if i == 113:
+        i += 1
+        continue
 
-	print(("{}".format(chr(i)), end="")
+    print(("{}".format(chr(i)), end="")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# islower - a function that determines case of input string
+# @c: input string to check
+
+def islower(c):
+    for ordinal in range(65, 98):
+        if ((c) == chr(c)):
+            print(True)
+        else:
+            print(False)
