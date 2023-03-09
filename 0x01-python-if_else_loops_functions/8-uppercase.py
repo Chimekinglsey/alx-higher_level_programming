d72fbeb49fb4497a3cd8c65a0df18d116e9cafa6
chimekinglsey <onlinekingsley@gmail.com>
2023-03-09 21:40:54 +0000

    first commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# islower - a function that determines case of input string
-# @c: input string to check
+# 8-uppercase.py
+# Brennan D Baraban <375@holbertonschool.com>
 
 
 def uppercase(str):
+    """Print a string in uppercase."""
     for c in str:
-         if ord(c) >= 97 and ord(c) <= 122:
-             c = chr(ord(c) - 32)
-         print("{}".format(c), end="")
-     print("")
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
