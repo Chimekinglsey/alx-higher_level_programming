103bd6e3b92176ccdf724b9645f938868878b21e
chimekinglsey <onlinekingsley@gmail.com>
2023-03-09 23:40:36 +0000

    FIrst commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+    result = 0
+    c = length - (length - 1)
+    if length == 1:
+        print("{}".format(0))
+    elif length == 2:
+        print("{}".format(int(argv[1])))
+    elif length > 2:    
+        while (c <= length-1):
+            result = result + int(argv[c])
+            c = c + 1
+        print("{}".format(result))
