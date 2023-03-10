4a6ca610408ca9e8b07366ca836d6add13bcc352
chimekinglsey <onlinekingsley@gmail.com>
2023-03-10 00:12:37 +0000

    first commit

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#Chime Kingsley @author
+
+if __name__ == "__main__":
+    import hidden_4
+    content = dir(hidden_4)
+    length = len(content)
+    count = 0
+    for count in len:
+    if (content[count][count] != "_"):
+        print("{}".format(content[count])
