9e3bdff6469448285d8e9841be8a928ccb760eab
chimekinglsey <onlinekingsley@gmail.com>
2023-03-10 01:08:33 +0000

    first commit

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
+    if len(argv) != 3:
+        print("{}".format('Usage: ./100-my_calculator.py <a> <operator> <b>'))
+        return 1
+    elif (op != '+' & op != '-' & op != '*' & op != '/'):
+        print('Unknown operator. Available operators: +, -, * and /')
+        return 1
+    elif op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif op == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
