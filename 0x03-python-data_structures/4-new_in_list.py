8acb96b3f059ceee35e3cfabd837f96ad0eb33f8
chimekinglsey <onlinekingsley@gmail.com>
2023-03-14 01:49:37 +0000

    first commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+# 4-new_in_list.py
+# Brennan D Baraban <375@holbertonschool.com>
+
 
 def new_in_list(my_list, idx, element):
-    """Replace an element at given index without modifying
-the original list"""
-    if isinstance(my_list, list):
-        lent = len(my_list)
-        if idx < 0:
-            return my_list.copy()
-        if idx > lent - 1:
-            return my_list.copy()
-        else:
-            copy = my_list.copy()
-            copy[idx] = element
-            return copy
+    """Replace an element in a copied list at a specific position."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return (copy)
