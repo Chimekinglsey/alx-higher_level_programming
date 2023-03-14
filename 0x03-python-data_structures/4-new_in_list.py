3d0585e8bd3019256852b74911f983bf67583d9e
chimekinglsey <onlinekingsley@gmail.com>
2023-03-14 00:53:48 +0000

    First COmmit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """Replace an element at given index without modifying 
+    """Replace an element at given index without modifying
 the original list"""
     lent = len(my_list)
     if idx < 0:
