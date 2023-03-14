c89518d1d6db0adec5f16c0e4db042052ea2d616
chimekinglsey <onlinekingsley@gmail.com>
2023-03-14 00:52:15 +0000

    First COmmit

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """Replace an element at given index without modifying 
+the original list"""
+    lent = len(my_list)
+    if idx < 0:
+        return my_list
+    if idx > lent - 1:
+        return my_list.copy
+    else:
+        copy = my_list.copy()
+        copy[idx] = element
+        return copy
