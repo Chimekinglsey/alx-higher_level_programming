d15a5d0bca8b6e8f4e018e729f685b0e83b44faa
chimekinglsey <onlinekingsley@gmail.com>
2023-04-01 14:21:41 +0000

    Did all, first commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,13 +36,13 @@ class Square:
     def position(self, value):
         """Setter method for position instance"""
         if type(value) is not tuple:
-            raise TypeError("position must be a tupple of 2 positive integers")
-        elif (value[0] or value[1]) is not int:
-            raise TypeError("position must be a tupple of 2 positive integers")
-        elif value[0] or value[1] < 0:
-            raise TypeError("position must be a tupple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif (type(value[0]) or type(value[1])) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] or value[1] is not > 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         elif value[0] or value[1] is None:
-            raise TypeError("position must be a tupple of 2 positive integers")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def my_print(self):
