c471dc79d1978939d26d51bb3536bc5c35f59409
chimekinglsey <onlinekingsley@gmail.com>
2023-04-04 01:36:02 +0000

    First commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -32,7 +32,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Sets the value for width"""
-        if not isinstance(value, int):
+        if type(value) not int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
