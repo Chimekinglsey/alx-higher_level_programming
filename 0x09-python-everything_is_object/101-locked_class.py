5a4a9aaf1e2d17e91cfab7161464ebf9572f0042
chimekinglsey <onlinekingsley@gmail.com>
2023-04-04 23:03:10 +0000

    First Commit

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+class LockedClass:
+    """__slots__ is used to create variable\
+only accessible through defined keyword"""
+    __slots__ = "first_name"
---
+++ 0x09-python-everything_is_object/test.py
@@ -0,0 +1,8 @@
+def increment(n):
+    return n + 1
+a = 1
+increment(a)
+print(a)
+print(increment(a))
+print(increment(3))
+
