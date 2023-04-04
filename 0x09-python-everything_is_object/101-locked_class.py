cf4110b1c17737368f1060838d18d5ab7fd3f7ec
chimekinglsey <onlinekingsley@gmail.com>
2023-04-04 23:27:13 +0000

    First Commit

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""MODULE 0"""
+
+
 class LockedClass:
+    """__slots helps to restrcit assignments to Objects\
+by disabling __dict__. only things seclared in slots are usable"""
     __slots__ = "first_name"
