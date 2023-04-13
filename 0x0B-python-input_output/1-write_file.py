fcc1246cc2cfa5432cd7893f663ab6c03a9261f9
chimekinglsey <onlinekingsley@gmail.com>
2023-04-13 03:04:01 +0000

    First commit

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,9 +4,9 @@
 
 def write_file(filename="", text=""):
     """
-    This function writes a string to a text file (UTF8) 
+    This function writes a string to a text file (UTF8)
     and returns the number of characters written
-    @Args: 
+    @Args:
     filename: Name of file to write to
     text: content to be written into file
     """
