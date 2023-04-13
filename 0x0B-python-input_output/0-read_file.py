85b4ec2c527612a2a6817f3b3a838665921742a8
chimekinglsey <onlinekingsley@gmail.com>
2023-04-13 02:41:41 +0000

    First COmmit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     this function reads a textfile and prints it
     @Args: filename
     """
-    with open(filename, 'r') as f:
+    with open(filename, encoding='utf-8') as f:
         print(f.read())
