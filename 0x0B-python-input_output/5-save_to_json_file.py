a73159d8fb18b14c6043e492ac778ef37c8473cb
chimekinglsey <onlinekingsley@gmail.com>
2023-04-13 17:36:11 +0000

    First COmmit

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,3 @@ def save_to_json_file(my_obj, filename):
     """Saves Object to filename"""
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
-    
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 6-load_from_json_file.py
+"""MODULE 7: CREATE OBJECT FROM JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """This function creates an a Python Object from a JSON file"""
+    with open(filename, 'r', encoding='utf-8') as jfile:
+        json.load(jfile)
