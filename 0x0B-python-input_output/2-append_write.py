432af4cd137a879dd216ce8ee8ad78e29baaff7d
chimekinglsey <onlinekingsley@gmail.com>
2023-04-13 03:10:08 +0000

    First Commit

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module 3: Append FUnction."""
+
+
+def append_write(filename="", text=""):
+    """
+    This function appends a string to the end of a text file (UTF8)
+    and returns the number of characters added
+    @Args:
+    filename: Name of file to append to
+    text: content to be added to file
+    """
+    with open(filename, mode='a', encoding='utf-8') as f:
+        f.seek(0)
+        f.write(text)
+        return (f.tell())
