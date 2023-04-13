01c3a109abda95a56d702b1fce7ff3310f44264a
chimekinglsey <onlinekingsley@gmail.com>
2023-04-13 18:39:43 +0000

    First Commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """MODULE 8: ADD ALL ARGS into a file and save as JSON file"""
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     try:
         items = load_from_json_file("add_item.json")
-    except Exception:
+    except FileNotFoundError:
         items = []
-    items.extend(sys.argv[1:])
+    items.extend(argv[1:])
     save_to_json_file(items, "add_item.json")
