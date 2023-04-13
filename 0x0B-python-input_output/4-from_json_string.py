e24e5faa2fc1dcd0e1b07237393c28875d63217b
chimekinglsey <onlinekingsley@gmail.com>
2023-04-13 05:47:04 +0000

    First COmmit

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-""" Module FOUR: JSON to Python Dict converter FUnction."""
+""" Module FIVE: JSON to Python Dict converter FUnction."""
 import json
 
 
 def to_json_string(my_obj):
     """
     This fucntion returns the Python object represented by a JSON string
-    of a given object
-    @Args: my_obj
     """
-    return json.loads(my_obj)
+    return(json.loads(my_obj))
