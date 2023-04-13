df1a32e11463333096d6577826bce7980b31c192
chimekinglsey <onlinekingsley@gmail.com>
2023-04-13 19:30:32 +0000

    FIrst commit

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""MODULE 10: STUDEN TO JSON CLASS"""
+
+
+class Student:
+    """This class defines a student first and lastname, + age"""
+    def __init__(self, first_name, last_name, age):
+        """we instantiate our pubic attributes for Student class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    @property
+    def to_json(self):
+        """retrieves the dictionary representation of Student class"""
+        return self.__dict__
