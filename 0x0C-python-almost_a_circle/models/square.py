14ee9cb749a49c14f54c6a615a589d38ac90d662
chimekinglsey <onlinekingsley@gmail.com>
2023-04-18 22:30:21 +0000

    New Commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,9 +4,18 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Inherits from rectangle class"""
+    """Inherits from rectangle class
+       must be initialized to inherit all attributes of rectangle
+       since a square is same as rectangle with equal w and h
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializing Square class with additional size attribute"""
+        """Initializing Square class with additional size attribute
+        Args:
+        @size: size of both width and height 
+        @x: value of x cordinate
+        @y: value of y cordinate
+        @id: object integer id
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
