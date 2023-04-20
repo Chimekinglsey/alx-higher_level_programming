bad8906d1a73910942b551794b9cc821180dff0a
chimekinglsey <onlinekingsley@gmail.com>
2023-04-20 02:04:20 +0000

    First coommit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,9 +22,8 @@ class Base:
     def to_json_string(list_dictionaries):
         """returns JSON string rep of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return '"[]"'
-        string = json.dumps(list_dictionaries)
-        return string
+            return "[]"
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
