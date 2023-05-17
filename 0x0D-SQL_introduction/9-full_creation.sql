ac61cf01811cce314c7b25019bbb97356f05687b
chimekinglsey <onlinekingsley@gmail.com>
2023-05-17 04:47:30 +0000

    final commits

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,18 @@
--- Write a script that creates a table second_table in the database
--- hbtn_0c_0 in your MySQL server and add multiples rows.
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
--- Inserting items
-INSERT INTO second_table(id, name, score), VALUES('1', 'John', '10');
-INSERT INTO second_table (id, name, score), VALUES('2', 'Alex', '3');
-INSERT INTO second_table (id, name, score), VALUES('3', 'Bob', '14');
-INSERT INTO second_table (id, name, score), VALUES('4', 'George', '8');
+-- CReate a new table
+-- DDL query to create a new table
+CREATE TABLE IF NOT EXISTS second_table
+(id INT,
+ name VARCHAR(256),
+ score INT);
+-- DML query to insert first row into the table
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10);
+-- DML query to insert second row into the table
+INSERT INTO second_table (id, name, score)
+VALUES (2, 'Alex', 3);
+-- DML quert to insert third row into the table
+INSERT INTO second_table (id, name, score)
+VALUES (3, 'Bob', 14);
+-- DML query to insert fourth row into the table
+INSERT INTO second_table (id, name, score)
+VALUES (4, 'George', 8);
