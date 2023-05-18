135f835238fd3a1964664cacc2dd57c468fe58ce
chimekinglsey <onlinekingsley@gmail.com>
2023-05-18 02:27:28 +0000

    newest commit

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,4 @@
---Write a script that creates the table id_not_null on your MySQL server.
--- id_not_null description: id INT with the default value 1 name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256) NOT NULL);
+-- create id not null
+-- DDL query to create table with id initialized in 1
+CREATE TABLE IF NOT EXISTS id_not_null(
+id INT DEFAULT 1, name VARCHAR(256) NOT NULL );
