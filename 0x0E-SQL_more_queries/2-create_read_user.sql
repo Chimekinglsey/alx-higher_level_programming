4deddfb26328870605a9b7502619204419ca725f
chimekinglsey <onlinekingsley@gmail.com>
2023-05-18 01:42:12 +0000

    First commit

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -7,6 +7,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creating user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- removing every privilege for user_0d_2
-GRANT USAGE ON *.* TO 'user_0d_2';
+GRANT USAGE ON `hbtn_0d_2`.* TO 'user_0d_2';
 -- granting select privileges for user
 GRANT SELECT ON `hbtn_0d_2` TO 'user_0d_2'@'localhost';
