6b9ab85a72f01f5eb05930b67624e3f1034c8643
chimekinglsey <onlinekingsley@gmail.com>
2023-05-18 02:35:11 +0000

    newest commit

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
---Write a script that creates the table unique_id on your MySQL server.
--- unique_id description: id INT with the default value 1 and must be unique
+-- create a table unique_id
+-- DDL query to create a table with default value 1 and must be unique
 CREATE TABLE IF NOT EXISTS unique_id(
 id INT DEFAULT 1 UNIQUE, name VARCHAR(256) NOT NULL);
