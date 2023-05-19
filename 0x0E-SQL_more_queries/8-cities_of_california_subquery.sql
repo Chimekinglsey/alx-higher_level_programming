a2e7067a3b6ecd32f6ab83b2b96e02d10c81a5f1
chimekinglsey <onlinekingsley@gmail.com>
2023-05-19 17:06:17 +0000

    New correction

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- DML sccript that lists all cities of California found
 -- in the hbtn_0d_usa database
-USE hbtn_0d_usa;
 --SELECT * FROM states WHERE 'name' = 'California' ASC AND 'id' = SELECT city_id FROM city where 'name' = 'California'
 SELECT cities.id, state.state_id, cities.name 
 FROM cities, states 
