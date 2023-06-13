019b1cb7aaed074e590a18567f1689dc94fa7815
chimekinglsey <onlinekingsley@gmail.com>
2023-06-13 22:39:39 +0000

    Just check

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const myArgs = process.argv[2];
+const parsed = parseInt(myArgs);
+const j = 0;
+const k = 'X';
+if (parsed) {
+  for (let i = 0; i < parsed; i++) {
+    console.log(k.repeat(parsed));
+  }
+} else if (!parsed) {
+  console.log('Missing size');
+}
