f48555c57de18ebbe839ca8aed3f8f5235f913e1
chimekinglsey <onlinekingsley@gmail.com>
2023-06-13 21:45:07 +0000

    Newest

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,4 +5,6 @@ if (!args[2] || (parseInt(args[2], 10) === 'NaN')) {
   console.log('Not a number');
 } else if (parseInt(args[2], 10)) {
   console.log('My number: ' + parseInt(args[2], 10));
+} else {
+  console.log('Not a number');
 }
