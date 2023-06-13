036ca8fb3f7bffcbb67e7792cd16b683e7312909
chimekinglsey <onlinekingsley@gmail.com>
2023-06-13 21:25:12 +0000

    Please Just work

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const args = process.argv;
 if (!args[2] || (parseInt(args[2], 10) === 'NaN')) {
   console.log('Not a number');
 } else if (parseInt(args[2], 10)) {
-  console.log('My Number: ' + parseInt(args[2], 10));
+  console.log('My number: ' + parseInt(args[2], 10));
 }
