261b9848ddf5798eab269fa6b4e43731a0cd3001
chimekinglsey <onlinekingsley@gmail.com>
2023-06-13 22:59:56 +0000

    New COmmit

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const a = parseInt(args[2]);
+const b = parseInt(args[3]);
+function add (a, b) {
+  const result = a + b;
+  return result;
+}
+console.log(add(a, b));
