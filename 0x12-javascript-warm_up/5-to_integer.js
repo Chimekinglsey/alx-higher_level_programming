b860a6d41982e80a3025e54fdf767ebd5d59173c
chimekinglsey <onlinekingsley@gmail.com>
2023-06-13 21:18:01 +0000

    New commit

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const args = process.argv;
+if (!args[2] || (parseInt(args[2], 10) === "NaN")) {
+	console.log("Not a number");
+} else if (parseInt(args[2], 10)) {
+	console.log("My Number: " + parseInt(args[2], 10));
+}
