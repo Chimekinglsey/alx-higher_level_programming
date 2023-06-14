afad4c275bb431554fa84354081fc97794a3ff39
chimekinglsey <onlinekingsley@gmail.com>
2023-06-14 01:47:14 +0000

    new advanced

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-exports.callMeMoby = function (x, callMeMoby)
-  {
+exports.callMeMoby = function (x, callMeMoby) {
 	  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+    callMeMoby();
   }
-}
-
+};
