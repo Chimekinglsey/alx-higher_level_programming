f8a8d4f8eba524a9da1a4272047cc2460b083e03
chimekinglsey <onlinekingsley@gmail.com>
2023-06-15 14:13:19 +0000

    new commit

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-exports.converter = function converter (base) {
-  return x => x.toString(base);
-};
+exports.converter = function (base) {
+	return function (x){
+		return x.toString(base);
+		}
+}
