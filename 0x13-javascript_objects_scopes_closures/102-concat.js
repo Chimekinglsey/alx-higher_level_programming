1ed45d996a4f089df8a9acd85bf6b72355c80f8b
chimekinglsey <onlinekingsley@gmail.com>
2023-06-15 16:43:16 +0000

    new commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 const args = process.argv;
-if (args.length === 4) {
+if (args.length === 5) {
   const readFile1 = fs.readFileSync(args[2], 'utf-8');
   const readFile2 = fs.readFileSync(args[3], 'utf-8');
   const fileC = readFile1 + readFile2;
-  fs.writeFileSync('fileC.txt', fileC, 'utf-8');
+  fs.writeFileSync(args[4], fileC, 'utf-8');
 }
