6ae9508dcbb1076347c61d1f0e9dbd6d67cc58a3
chimekinglsey <onlinekingsley@gmail.com>
2023-06-15 15:21:19 +0000

    new commit

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { list } = require('./100-data');
-const newList = list.map((ele, index) => ele * index);
+const newList = list.map((ele, index, list) => ele * index);
 
 console.log(list);
 console.log(newList);
