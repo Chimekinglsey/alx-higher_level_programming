a504c7e8c8c6a6972e12c63794ffeca3c812f84d
chimekinglsey <onlinekingsley@gmail.com>
2023-06-19 23:51:12 +0000

    new commit

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,11 +8,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    state_name = argv[4]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name.like('%a%')).all()
-    for row in state:
-        print("{}: {}".format(row.id, row.name))
+    state = session.query(State).filter(State.name = %s, (state_name)).count()
+    print(state)
