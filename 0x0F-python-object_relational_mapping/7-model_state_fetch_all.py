d6c46ab2b28ca37b0bafce02e144fc787106dabd
chimekinglsey <onlinekingsley@gmail.com>
2023-06-19 22:31:15 +0000

    new commit

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -21,6 +21,6 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State).order_by(State.id).all()
+    query = session.query(State).order_by(State.id)
     for row in query:
         print("{}: {}".format(row.id, row.name))
