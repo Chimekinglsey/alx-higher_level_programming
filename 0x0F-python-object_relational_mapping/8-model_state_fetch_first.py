768f3775be44ac4552917bcfc1221764264b4eec
chimekinglsey <onlinekingsley@gmail.com>
2023-06-19 23:00:57 +0000

    new commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Accessing external database called `states` using SQLAlchemy
+"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).fetchone()
+    for row in state:
+        print("{}: {}".format(row.id, row.name))
