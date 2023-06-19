840b648c8eb5203f1ce13b1b02cd186097fb3510
chimekinglsey <onlinekingsley@gmail.com>
2023-06-19 16:03:44 +0000

    new commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         port=3306, db=db_name
      )
     query = "SELECT * FROM states WHERE  states.name \
-                LIKE BINARY '{}' ORDER BY state.id;".format(name_searched)
+                LIKE BINARY '{}' ORDER BY states.id;".format(name_searched)
     cursor = db.cursor()
     cursor.execute(query)
     result = cursor.fetchall()
