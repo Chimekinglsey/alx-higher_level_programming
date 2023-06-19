a82df1da8bff015eeeb3c9bb62609211546944dc
chimekinglsey <onlinekingsley@gmail.com>
2023-06-19 22:24:57 +0000

    new commit

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,21 +3,21 @@
 Accessing external database called `states` using SQLAlchemy
 """
 from model_state import Base, State
-from sys import argv
-from sqlachemy import create_engine
-from sqlachemy.orm import sessionmaker
+from sys import argv, exit
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     if len(argv) != 4:
         print("Usage: ./7-model_state... <username> <passwd> <dbname>")
-        return (0)
+        exit (1)
     username = argv[1]
     password = argv[2]
     db_name = argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/\
-                    {}').format(username, passowrd, db_name)
+                    {}'.format(username, passowrd, db_name))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
