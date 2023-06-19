ac5c32602789478d1fede715f51a3fc4816e6175
chimekinglsey <onlinekingsley@gmail.com>
2023-06-19 23:38:34 +0000

    new commit

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name.like('%a%'))
+    state = session.query(State).filter(State.name.like('%a%')).all()
     for row in state:
         print("{}: {}".format(state.id, state.name))
