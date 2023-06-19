1435a0594834eabffa5383c84c2f17950d1e7f3c
chimekinglsey <onlinekingsley@gmail.com>
2023-06-19 20:16:08 +0000

    new commit

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,12 +2,9 @@
 """
 Using sqlalchemy to implement MySQLdb server connection
 """
-import sqlalchemy
 from sqlalchemy.ext.declaratives import declarative_base
 from sqlalchemy import Integer, String, create_engine, Column
 
-engine = create_engine('mysql://chime:chime@localhost:3306/mysqlalchemy')
-connection = engine.connect()
 Base = declarative_base()
 
 
