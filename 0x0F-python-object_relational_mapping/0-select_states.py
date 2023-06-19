a3d20b54a7f62ff7ddacac3387dab1c24612cf3e
chimekinglsey <onlinekingsley@gmail.com>
2023-06-19 13:40:35 +0000

    new commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,21 +3,19 @@ import MySQLdb
 from sys import argv
 
 """We are importing MySQLdb module from Python library to aid our
-	transactions"""
+        transactions"""
 user = argv[1]
 password = argv[2]
 db_name = argv[3]
 
-db = MySQLdb.connect(host='localhost', user=user, passwd=password,\
-port=3306, db=db_name)
+db = MySQLdb.connect(
+    host='localhost', user=user, passwd=password,
+    port=3306, db=db_name)
 curs = db.cursor()
 
 select_query = "SELECT * FROM states ORDER BY id ASC"
 result = cursor.execute(select_query).fetchall()
 for row in result:
-	print(row)
+    print(row)
 cursor.close()
 db.close()
-
-
-
