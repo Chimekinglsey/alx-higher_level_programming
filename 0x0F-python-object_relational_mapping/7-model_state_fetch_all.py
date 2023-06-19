ca0e713090f2def6ab512a73851cc75b176994b8
chimekinglsey <onlinekingsley@gmail.com>
2023-06-19 22:47:59 +0000

    new commit

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,12 +12,13 @@ if __name__ == '__main__':
     if len(argv) != 4:
         print("Usage: ./7-model_state... <username> <passwd> <dbname>")
         exit(1)
+    '''
     username = argv[1]
     password = argv[2]
     db_name = argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/\
-                    {}'.format(username, password, db_name))
+    '''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format\
+    (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
