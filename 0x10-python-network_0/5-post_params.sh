e560ca3b0a660e89666fbbb9b4fcde81cf9f105e
chimekinglsey <onlinekingsley@gmail.com>
2023-06-30 09:27:04 +0000

    Post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send POST requests with -H parameters
-curl -sX POST -H "email:test@gmail.com" -H "subject:'I will always be here for PLD'" $1
+curl -sX POST -H "email:test@gmail.com" -H "subject:I will always be here for PLD" $1
