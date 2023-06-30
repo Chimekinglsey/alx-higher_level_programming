93ee38e38d76e632e9925580dd9a5662aa4ee527
chimekinglsey <onlinekingsley@gmail.com>
2023-06-30 08:51:15 +0000

    display Head regardless of redirect

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This takes a URL, sends GET request to it and displays the body of the response
+# This takes a URL, sends GET request to it and displays the body of the response following all redirects
 curl -sL $1
