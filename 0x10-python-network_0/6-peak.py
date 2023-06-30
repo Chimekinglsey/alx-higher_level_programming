bf042ef971323a083ebe78ea40ba6549f27e2edd
chimekinglsey <onlinekingsley@gmail.com>
2023-06-30 11:02:37 +0000

    Peak Sort

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Find the Peak of a given an array"""
+
+
+def find_peak(list_of_integers):
+    """This function takes a list, sorts it and returns the peak value"""
+    sorted_list = sorted(list_of_integers)
+    if len(sorted_list) > 0:
+        return sorted_list[len(sorted_list)-1]
