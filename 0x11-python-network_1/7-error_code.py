1f1ca4e723201e06da84ad405562133a29394896
chimekinglsey <onlinekingsley@gmail.com>
2023-07-03 02:55:56 +0000

    Status code error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,7 @@ import requests
 if __name__ == "__main__":
     url = argv[1]
     response = requests.get(url)
-    print(response.text)
     if response.status_code >= 400:
         print("Error code: ", response.status_code)
+    else:
+        print(response.text)
