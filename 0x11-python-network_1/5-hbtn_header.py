395e72432000e667f2fa9e8a8f143f2873452570
chimekinglsey <onlinekingsley@gmail.com>
2023-07-03 02:17:56 +0000

    Requests is cool

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is MODULE:1 - Using urllib and sys
+1. Response header value #0
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    print(req.headers['X-Request-Id'])
