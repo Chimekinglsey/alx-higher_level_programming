e33ef31419875025ef5c9e8a88fff9353f13e853
chimekinglsey <onlinekingsley@gmail.com>
2023-07-03 01:29:59 +0000

    testing same encoder and decoder

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,9 +10,9 @@ from urllib import request, parse
 if __name__ == "__main__":
     url = argv[1]
     email = parse.urlencode({'email': argv[2]})
-    data = email.encode('ascii')
+    data = email.encode('utf-8')
     req = request.Request(url, data)
     with request.urlopen(req) as resp:
-        decode = resp.decode('utf-8')
-        display = decode.read()
+        reader = resp.read()
+        display = reader.decode('utf-8')
         print(display)
