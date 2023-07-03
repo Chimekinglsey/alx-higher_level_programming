ad37694e1e8252b14456c89ba7381d35e7819c3d
chimekinglsey <onlinekingsley@gmail.com>
2023-07-03 00:58:14 +0000

    Using Urllib

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is MODULE:1 - Using urllib and sys
+1. Response header value #0
+"""
+from sys import argv
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as req:
+        info = req.info()
+        print(info['X-Request-Id'])
