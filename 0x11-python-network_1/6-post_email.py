3adb9ca566ea69af1a8036465a307f726b01fe53
chimekinglsey <onlinekingsley@gmail.com>
2023-07-03 02:33:57 +0000

    Will it work as email?

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is MODULE:6 - This script takes in a URL + email, sends POST
+request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = requests.get(url, params=({'email': argv[2]}))
+    print(req)
