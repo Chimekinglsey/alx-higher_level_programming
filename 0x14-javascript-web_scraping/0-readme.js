fe62023cdc6d413efe3919a54119faecc3700c31
chimekinglsey <onlinekingsley@gmail.com>
2023-07-26 16:30:22 +0000

    Does it read?

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require("fs");
+const arg = process.argv[2];
+
+let fileContent = fs.readFile(arg, 'utf-8', (err, data) => {
+    if (err) {
+            console.log(err);
+        }
+    else {
+        console.log(data);
+        }
+    })
+
