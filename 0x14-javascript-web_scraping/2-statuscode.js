b15510c7f412ae4d25b579dd1ce45694318452c5
chimekinglsey <onlinekingsley@gmail.com>
2023-07-26 20:54:37 +0000

    Request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const req = require('request');
 const url = process.argv[2];
 req.get(url).on('response', (response) => {
-    console.log(`code: ${response.statusCode}`)
-    })
+  console.log(`code: ${response.statusCode}`);
+});
