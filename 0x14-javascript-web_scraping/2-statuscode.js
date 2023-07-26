2d094f234f0946e7b67f7d79f52cafda7e01d32c
chimekinglsey <onlinekingsley@gmail.com>
2023-07-26 17:56:41 +0000

    using requests

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+const arg = process.argv[2];
+
+request(arg, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  }
+  console.log('code: ', response.statusCode);
 });
