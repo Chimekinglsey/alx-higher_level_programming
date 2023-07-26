2a8431a3e61efb0b795821f5758139d068f4ab1a
chimekinglsey <onlinekingsley@gmail.com>
2023-07-26 20:12:31 +0000

    Read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const arg1 = process.argv[2];
-fs.readFile(arg1, 'utf-8', function (error, data) {
+fs.readFile(arg1, 'utf-8', (error, data) => {
   if (error) {
     console.error(error);
   } else {
