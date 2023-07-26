c3e79f0cd1c1d85f89660147545b30c62d928213
chimekinglsey <onlinekingsley@gmail.com>
2023-07-26 19:11:11 +0000

    removed

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const fs = require('fs');
-const file = process.argv[2];
-const arg = process.argv[3];
-fs.writeFile(file, arg, 'utf-8', (error) => {
-  if (error) {
-    console.log(error);
-  }
-});
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const connect = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
-
-connect(url, function (_err, _res, body) {
-  body = JSON.parse(body);
-  console.log(body.title);
-});
