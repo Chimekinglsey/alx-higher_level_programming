48ff1a19ec5563840265c23be2ac3cc9ad6495d0
chimekinglsey <onlinekingsley@gmail.com>
2023-07-26 16:57:45 +0000

    fixed requirement error display

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-const fs = require("fs");
-const arg = process.argv[2];
+const fs = require('fs');
+const arg = process.argv[2]; // process.argv[0] is "node", process.argv[1] is the script file, so the first argument is at index 2.
 
-let fileContent = fs.readFile(arg, 'utf-8', (err, data) => {
-    if (err) {
-            console.log(err);
-        }
-    else {
-        console.log(data);
-        }
-    })
-
+fs.readFile(arg, 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
